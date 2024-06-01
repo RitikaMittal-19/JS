76ffab9bf1ae5b1c45b7ed6b10d03d43889ae080
RitikaMittal-19 <2023kuec2062@iiitkota.ac.in>
2024-06-01 16:00:09 +0000

    datatype

---
+++ 01_basic/02_var.js
@@ -0,0 +1,18 @@
+"use strict";  //standard version of js
+//alert()      //popup
+console.table()
+let v = 15;
+let a="krfdf"
+let b = false
+
+//number=>2 to power 53
+//bigint
+//string
+//boolean
+//null->object
+//undefined
+//symbol=>unique
+
+
+//object: console.log(typeOf )
+//null
